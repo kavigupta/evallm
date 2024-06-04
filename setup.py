ef19da458c451369dfde7807a0661cf7e41eaea0
Kavi Gupta <kavi@berkeley.edu>
2024-06-04 01:16:36 -0400

    update

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setuptools.setup(
     ],
     python_requires=">=3.10",
     install_requires=["numpy==1.26.3", "automata-lib==8.3.0"],
-    # optional visual dependencies
     extras_require={
         "visual": ["automata-lib[visual]==8.3.0"],
     },
