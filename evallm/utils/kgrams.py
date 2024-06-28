3bdfc973d3e36e896d351d0ae35f018c327e70b8
Kavi Gupta <kavi@berkeley.edu>
2024-06-27 17:43:43 -0700

    multi kgrams

--- evallm/utils/kgrams.py
+++ evallm/utils/kgrams.py
@@ -1,26 +1,36 @@
-def longest_terminal_repeated_kgram(s):
+def longest_terminal_repeated_kgrams(s):
     """
     Get the longest repeated k-gram that satisfies the property s.endswith(kgram)
     """
     k = 1
     indices = [i + 1 for i in range(len(s) - 1) if s[i] == s[-1]]
+    results = []
     while True:
+        results.append((k, s[-k:], [i - k for i in indices]))
         k += 1
         new_indices = [i for i in indices if i - k >= 0 and s[i - k] == s[-k]]
         if len(new_indices) < 1:
-            k -= 1
-            return k, s[-k:], [i - k for i in indices]
+            return results
         indices = new_indices
+
+
+def predict_from_sequence_based_on_kgram(seq):
+    results = []
+    for k, _, idxs in longest_terminal_repeated_kgrams(seq):
+        preds = [seq[i + k] for i in idxs]
+        print(preds)
+        num_ones = sum(1 for x in preds if x == 1)
+        if num_ones * 2 > len(preds):
+            results.append(1)
+        elif num_ones * 2 < len(preds):
+            results.append(0)
+        else:
+            # break ties by whatever the most recent match was
+            results.append(preds[-1])
+    return results[-1]
 
 
 def predict_based_on_kgram(inp, out):
     prefix = [x for i, o in zip(inp, out) for x in [i, o]][:-1]
-    k, _, idxs = longest_terminal_repeated_kgram(prefix)
-    preds = [prefix[i + k] for i in idxs]
-    num_ones = sum(1 for x in preds if x == 1)
-    if num_ones * 2 > len(preds):
-        return 1
-    if num_ones * 2 < len(preds):
-        return 0
-    # break ties by whatever the most recent match was
-    return preds[-1]
+
+    return predict_from_sequence_based_on_kgram(prefix)
--- tests/kgram_test.py
+++ tests/kgram_test.py
@@ -6,22 +6,76 @@ import evallm
 class TestLongestKgram(unittest.TestCase):
     def test_basic(self):
         self.assertEqual(
-            evallm.utils.longest_terminal_repeated_kgram("abcdefgabc"),
-            (3, "abc", [0]),
+            evallm.utils.longest_terminal_repeated_kgrams("abcdefgabc"),
+            [(1, "c", [2]), (2, "bc", [1]), (3, "abc", [0])],
         )
 
     def test_no_repeats(self):
         self.assertEqual(
-            evallm.utils.longest_terminal_repeated_kgram("abcdefg"), (1, "g", [])
+            evallm.utils.longest_terminal_repeated_kgrams("abcdefg"), [(1, "g", [])]
         )
 
     def test_with_repeated_character(self):
         self.assertEqual(
-            evallm.utils.longest_terminal_repeated_kgram("abcaaaa"), (3, "aaa", [3])
+            evallm.utils.longest_terminal_repeated_kgrams("abcaaaa"),
+            [(1, "a", [0, 3, 4, 5]), (2, "aa", [3, 4]), (3, "aaa", [3])],
         )
 
     def test_repeated_thrice(self):
         self.assertEqual(
-            evallm.utils.longest_terminal_repeated_kgram("abcdeabcfgabc"),
-            (3, "abc", [0, 5]),
+            evallm.utils.longest_terminal_repeated_kgrams("abcdeabcfgabc"),
+            [(1, "c", [2, 7]), (2, "bc", [1, 6]), (3, "abc", [0, 5])],
+        )
+
+    def test_multiple_prefixes(self):
+        self.assertEqual(
+            evallm.utils.longest_terminal_repeated_kgrams(
+                "bcd ef def bcdef cdef abcdef"
+            ),
+            [
+                (1, "f", [5, 9, 15, 20]),
+                (2, "ef", [4, 8, 14, 19]),
+                (3, "def", [7, 13, 18]),
+                (4, "cdef", [12, 17]),
+                (5, "bcdef", [11]),
+            ],
+        )
+
+    def test_with_heterogenous_values(self):
+        self.assertEqual(
+            evallm.utils.longest_terminal_repeated_kgrams(
+                ["c", 0, "b", 0, "c", 0, "b", 0, "c", 1, "b", 0, "c"]
+            ),
+            [
+                (1, ["c"], [0, 4, 8]),
+                (2, [0, "c"], [3, 7]),
+                (3, ["b", 0, "c"], [2, 6]),
+            ],
+        )
+
+
+class TestPredictBasedOnKgram(unittest.TestCase):
+    def test_basic(self):
+        # kgram b0c -> 0, even though c -> 1
+        self.assertEqual(
+            evallm.utils.predict_from_sequence_based_on_kgram(
+                ["b", 0, "c", 0, "c", 1, "b", 0, "c"]
+            ),
+            0,
+        )
+
+    def test_tie(self):
+        # kgram b0c -> 0 then b0c -> 1, so tie
+        self.assertEqual(
+            evallm.utils.predict_from_sequence_based_on_kgram(
+                ["x", 0, "b", 0, "c", 0, "y", 0, "b", 0, "c", 1, "z", 0, "b", 0, "c"]
+            ),
+            1,
+        )
+        # kgram b0c -> 1 then b0c -> 0, so tie
+        self.assertEqual(
+            evallm.utils.predict_from_sequence_based_on_kgram(
+                ["x", 0, "b", 0, "c", 1, "y", 0, "b", 0, "c", 0, "z", 0, "b", 0, "c"]
+            ),
+            0,
         )
