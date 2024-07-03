bec4112584fa86940d5c71f14c203c4bb72ebf6c
Kavi Gupta <kavi@berkeley.edu>
2024-07-03 10:54:30 -0700

    formatting

--- evallm/sample_dfa/sample_dfa.py
+++ evallm/sample_dfa/sample_dfa.py
@@ -1,5 +1,6 @@
 import numpy as np
 from automata.fa.dfa import DFA
+
 from evallm.sample_dfa.naive_sample import naively_sample_dfa
 from evallm.sample_dfa.reachable_sample import sample_reachable_dfa
 from evallm.utils.construct import construct
