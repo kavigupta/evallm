4e5d189749db16805294411bbd00dac3e5ada9f2
Kavi Gupta <kavi@berkeley.edu>
2024-09-30 18:28:34 -0400

    update basic sequence prompt (#14)

--- scripts/transducer_experiment.py
+++ scripts/transducer_experiment.py
@@ -1,11 +1,10 @@
 import evallm
-from evallm.prompting.transducer_prompt import (
-    ChainOfThoughtPromptRealExampleNoExplanation,
-)
+from evallm.prompting.transducer_prompt import BasicSequencePrompt
 
 evallm.experiments.transducer_experiment.chatgpt_transducer_experiments(
-    "gpt-4o-mini-2024-07-18",
-    lambda num_sequence_symbols, sample_dfa_spec: ChainOfThoughtPromptRealExampleNoExplanation(
-        num_sequence_symbols, sample_dfa_spec, 1
+    "gpt-4o-2024-05-13",
+    cot_prompt=lambda num_sequence_symbols, sample_dfa_spec: BasicSequencePrompt(
+        num_sequence_symbols, version=3
     ),
+    num_states_options=(3,),
 )
