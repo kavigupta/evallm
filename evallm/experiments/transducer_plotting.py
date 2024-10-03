39b8a4d8e6ab7673cfc2db80acc57c1ce323ad50
Kavi Gupta <kavi@berkeley.edu>
2024-10-02 22:22:57 -0700

    extract plotting code into its own file

---
+++ evallm/experiments/transducer_plotting.py
@@ -0,0 +1,117 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import numpy as np
+
+from evallm.utils.bootstrap import boostrap_mean
+
+
+def plot_absolute_results(ax, which_llm, result_by_length, *, ignore_na):
+    plot_model_result(ax, which_llm, result_by_length, ignore_na, "black")
+    plot_baselines(ax, result_by_length)
+    ax.set_title(which_llm)
+
+
+def plot_result(ax, result_by_length, compute_outcome, **kwargs):
+    lengths = sorted(result_by_length)
+    results = (
+        100
+        * np.array(
+            [
+                [compute_outcome(r) for r in result_by_length[length]]
+                for length in lengths
+            ]
+        ).T
+    )
+    lo, hi = boostrap_mean(results)
+    ax.plot(lengths, results.mean(0), **kwargs)
+    ax.fill_between(
+        lengths, lo, hi, alpha=0.3, **{k: v for k, v in kwargs.items() if k == "color"}
+    )
+
+
+def plot_model_result(ax, which_llm, result_by_length, ignore_na, color):
+
+    return plot_result(
+        ax,
+        result_by_length,
+        lambda r: (
+            r.success_rate_binary_ignore_na if ignore_na else r.success_rate_binary
+        ),
+        color=color,
+        marker="o",
+        label=which_llm,
+    )
+
+
+def plot_baselines(ax, result_by_length):
+    ngrams = range(1, 1 + 5)
+    count = len(ngrams) + 1
+    linestyles = ["--", "-.", ":"] * 10
+    colors = [mpl.colors.hsv_to_rgb((i / count, 0.5, 0.5)) for i in range(count)]
+    plot_result(
+        ax,
+        result_by_length,
+        lambda r: r.null_success_rate,
+        color=colors.pop(0),
+        linestyle=linestyles.pop(0),
+        label="null",
+    )
+    for ngram in ngrams:
+        plot_result(
+            ax,
+            result_by_length,
+            lambda r, ngram=ngram: r.kgram_success_rates_each[ngram - 1],
+            color=colors.pop(0),
+            linestyle=linestyles.pop(0),
+            label=f"{ngram}gram",
+        )
+    ax.legend()
+    ax.set_xlabel("Sequence length")
+    ax.set_ylabel("Success rate [%]")
+    ax.grid()
+
+
+def plot_all_absolute_results(results, num_states, *, ignore_na):
+    _, axs = plt.subplots(
+        1,
+        len(results),
+        figsize=(5 * len(results), 5),
+        tight_layout=True,
+        facecolor="white",
+    )
+    axs = [axs] if len(results) == 1 else axs.flatten()
+    for ax, model_name in zip(axs, results):
+        plot_absolute_results(
+            ax, model_name, results[model_name][num_states], ignore_na=ignore_na
+        )
+    plt.suptitle(f"Prediction of {num_states}-state DFA")
+
+
+def plot_all_absolute_results_single_graph(results, num_states, *, ignore_na):
+    plt.figure(
+        figsize=(8, 8),
+        tight_layout=True,
+        facecolor="white",
+        dpi=200,
+    )
+    for i, model_name in enumerate(results):
+        plot_model_result(
+            plt.gca(),
+            model_name,
+            results[model_name][num_states],
+            ignore_na=ignore_na,
+            color=f"C{i}",
+        )
+    plot_baselines(plt.gca(), results[list(results)[0]][num_states])
+
+
+def plot_relative_results(relative, name, ax=None):
+    if ax is None:
+        ax = plt.gca()
+    for k in relative:
+        ax.plot(relative[k].index, relative[k] * 100, label=f"{k} states")
+    ax.legend()
+    ax.set_xlabel("Sequence Length")
+    ax.set_ylabel(f"Meets {name} %")
+    ax.axhline(50, color="black")
+    ax.grid()
