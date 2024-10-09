4d4165c36c86718c29d3cb880880998c47541703
Kavi Gupta <kavi@berkeley.edu>
2024-10-09 14:07:09 -0400

    explanation sequence prompt (#27)

--- evallm/prompting/transducer_prompt.py
+++ evallm/prompting/transducer_prompt.py
@@ -8,6 +8,8 @@ from evallm.sample_dfa.transduce import transduce
 
 from .prompter import Prompter
 
+ANSWER_PATTERN = re.compile(r"<answer>([01])</answer>")
+
 
 class TransducerPrompter(Prompter):
 
@@ -108,7 +110,6 @@ class BasicInstructionTransducerPrompter(CleanTransducerPrompter):
 
 class ChainOfThoughtPrompt(TransducerPrompter):
 
-    ANSWER_PATTERN = re.compile(r"<answer>([01])</answer>")
     version = 3
 
     def display(self):
@@ -138,7 +139,7 @@ class ChainOfThoughtPrompt(TransducerPrompter):
 
     def get_numeric_answer(self, message_content):
         answer_tag_result = [
-            x.group(1) for x in self.ANSWER_PATTERN.finditer(message_content)
+            x.group(1) for x in ANSWER_PATTERN.finditer(message_content)
         ]
         if answer_tag_result:
             return int(answer_tag_result[-1])
@@ -248,3 +249,68 @@ class BasicSequencePrompt(TransducerPrompter):
     def score_completion(self, output, choice):
         numeric = self.get_numeric_answer(choice.message.content)
         return numeric_answer_to_confusion(output, numeric)
+
+
+class SequencePromptWithExplanation(BasicSequencePrompt):
+
+    version = 0
+
+    def __init__(self, num_symbols, num_states):
+        super().__init__(num_symbols)
+        self.num_states = num_states
+
+    @classmethod
+    def for_setting(cls, setting_kwargs):
+        return cls(
+            setting_kwargs["num_sequence_symbols"],
+            setting_kwargs["num_states"],
+        )
+
+    def display(self):
+        return f"SequencePromptWithExplanation({self.num_symbols}, {self.num_states}, {self.version})"
+
+    def display_prompt(self, inp, out, is_chat):
+        assert is_chat, "for now, we only support chat systems for this prompter"
+        return dict(
+            system="",
+            user="A DFA is a finite-state machine that accepts or rejects a given string of symbols,"
+            + " by running through a n-state sequence uniquely determined by the string."
+            + "\n\n"
+            + f"I have a {self.num_states}-state DFA model that outputs either 0 or 1 after each element"
+            + ' I input. 1 indicates that the input string thus far results in a "valid" state, and 0'
+            + " indicates that it does not. I collect the inputs and outputs into an input sequence and"
+            + " an output sequence. Infer the underlying DFA model to predict the next integer in the"
+            + f" output sequence. {self.terminal_instruction()}"
+            + "\n\n"
+            + "Input sequence: "
+            + "".join([f"{x}, " for x in inp])[0:-2]
+            + "\n"
+            + "Output sequence: "
+            + "".join([f"{int(x)}, " for x in out[:-1]]),
+        )
+
+    def terminal_instruction(self):
+        return "Only output the next output and nothing else."
+
+
+class SequencePromptWithExplanationChainOfThought(SequencePromptWithExplanation):
+
+    version = 1
+
+    def display(self):
+        return f"SequencePromptWithExplanationChainOfThought({self.num_symbols}, {self.num_states}, {self.version})"
+
+    def terminal_instruction(self):
+        return (
+            "Reason step by step, and then output the next output"
+            " integer using <output> tags, like <output>0</output>."
+        )
+
+    def get_numeric_answer(self, message_content):
+        return ChainOfThoughtPrompt.get_numeric_answer(self, message_content)
+
+    def prompt_kwargs(self):
+        return {
+            "max_tokens": 5000,
+            "temperature": 0.0,
+        }
