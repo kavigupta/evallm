af80ecfd0644bcd4207035f9ac91de19c74348e5
Kavi Gupta <kavi@berkeley.edu>
2024-11-20 12:39:52 -0800

    clean up, delete old prompts

--- evallm/prompting/transducer_prompt.py
+++ evallm/prompting/transducer_prompt.py
@@ -96,71 +96,29 @@ class BasicInstructionTransducerPrompter(CleanTransducerPrompter):
         )
 
     def display_prompt(self, inp, out, is_chat):
-        assert (
-            not is_chat
-        ), "for now, we only support non-chat systems for this prompter"
-        pattern = super().display_prompt(inp, out, is_chat)
-        result = (
-            "A DFA was used to create these outputs given a random sequence of inputs. "
-            f"Your job is to fill in the last output:\n{pattern}"
-        )
-        if self.strip:
-            result = result.strip()
-        return result
-
-
-class ChainOfThoughtPrompt(TransducerPrompter):
-
-    version = 3
-
-    def display(self):
-        return f"ChainOfThoughtPrompt({self.num_symbols}, {self.version})"
-
-    def display_prompt(self, inp, out, is_chat):
-        assert is_chat, "for now, we only support chat systems for this prompter"
-        return dict(
-            system="You are a question answering system. For each question, think step by step and"
-            + " place your answer between tags like <answer>0</answer> or <answer>1</answer>."
-            + " MAKE SURE TO PUT YOUR ANSWER IN ANSWER TAGS or you will get NO CREDIT.\n"
-            + "QUESTION:\nWhat is 20*2?\n"
-            + "ANSWER:\nTo solve this problem, we need to multiply 20 by 0. We can accomplish this"
-            + " via noticing that anything multiplied by 0 is 0. <answer>0</answer>\n",
-            user="QUESTION:\nA DFA was used to create these outputs given a random"
-            + " sequence of inputs. "
-            + "Your job is to fill in the last output:\n"
-            + serialize_transducer_prompt(inp, out)
-            + "out: _\n"
-            + "\nANSWER:\n",
+        raise NotImplementedError(
+            "This should not actually be used. "
+            "It only exists as a default for caching in ngram models, etc."
         )
 
-    def prompt_kwargs(self):
-        return {
-            "max_tokens": 5000,
-        }
 
-    def get_numeric_answer(self, message_content):
-        answer_tag_result = [
-            x.group(1) for x in ANSWER_PATTERN.finditer(message_content)
-        ]
-        if answer_tag_result:
-            return int(answer_tag_result[-1])
-        last_index_1 = message_content.rfind("1")
-        last_index_0 = message_content.rfind("0")
-        if last_index_0 == -1 and last_index_1 == -1:
-            # neither occurs in the message
-            return None
-        if last_index_1 == -1:
-            # only 0 occurs in the message
-            return 0
-        if last_index_0 == -1:
-            # only 1 occurs in the message
-            return 1
-        # both occur in the message, return the one that occurs last
-        return 1 if last_index_1 > last_index_0 else 0
-
-    def score_completion(self, output, choice):
-        numeric = self.get_numeric_answer(choice.message.content)
-        return numeric_answer_to_confusion(output, numeric)
+def parse_cot_answer(message_content):
+    answer_tag_result = [x.group(1) for x in ANSWER_PATTERN.finditer(message_content)]
+    if answer_tag_result:
+        return int(answer_tag_result[-1])
+    last_index_1 = message_content.rfind("1")
+    last_index_0 = message_content.rfind("0")
+    if last_index_0 == -1 and last_index_1 == -1:
+        # neither occurs in the message
+        return None
+    if last_index_1 == -1:
+        # only 0 occurs in the message
+        return 0
+    if last_index_0 == -1:
+        # only 1 occurs in the message
+        return 1
+    # both occur in the message, return the one that occurs last
+    return 1 if last_index_1 > last_index_0 else 0
 
 
 def numeric_answer_to_confusion(output, numeric):
@@ -172,35 +130,6 @@ def numeric_answer_to_confusion(output, numeric):
     else:
         confusion[output, :] = 0.5
     return confusion
-
-
-class ChainOfThoughtPromptRealExampleNoExplanation(ChainOfThoughtPrompt):
-
-    version = 0
-
-    def __init__(self, num_symbols, sample_dfa_spec, num_samples):
-        super().__init__(num_symbols)
-        self.sample_dfa_spec = sample_dfa_spec
-        self.num_samples = num_samples
-        rng = np.random.RandomState(2**32 - 2)
-        self.samples = [sample_dfa(sample_dfa_spec, rng) for _ in range(num_samples)]
-        self.io = [self.input_output(dfa, rng) for dfa in self.samples]
-        self.user_prompts = [
-            ChainOfThoughtPrompt.display_prompt(self, inp, out, is_chat=True)["user"]
-            + f"<answer>{int(out[-1])}</answer>\n\n"
-            for (inp, out) in self.io
-        ]
-
-    def display(self):
-        return (
-            f"ChainOfThoughtPromptRealExamples({self.num_symbols},"
-            f" {self.sample_dfa_spec}, {self.num_samples}, {self.version})"
-        )
-
-    def display_prompt(self, inp, out, is_chat):
-        prompt = ChainOfThoughtPrompt.display_prompt(self, inp, out, is_chat)
-        prompt["user"] = "".join(self.user_prompts) + prompt["user"]
-        return prompt
 
 
 class BasicSequencePrompt(TransducerPrompter):
@@ -356,7 +285,7 @@ class SequencePromptWithExplanationChainOfThought(SequencePromptWithExplanation)
         )
 
     def get_numeric_answer(self, message_content):
-        return ChainOfThoughtPrompt.get_numeric_answer(self, message_content)
+        return parse_cot_answer(self, message_content)
 
     def prompt_kwargs(self):
         return {
