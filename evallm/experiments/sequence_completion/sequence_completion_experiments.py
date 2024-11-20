9d88b3b3b91fe3334eaff4d6c8878287a0734db2
Kavi Gupta <kavi@berkeley.edu>
2024-11-20 12:11:46 -0800

    add sequence completion experiments

---
+++ evallm/experiments/sequence_completion/sequence_completion_experiments.py
@@ -0,0 +1,168 @@
+from typing import Counter
+import numpy as np
+from permacache import permacache
+import tqdm.auto as tqdm
+from evallm.enumerate_dfa.pack_dfa import pack_dfa
+from evallm.experiments.sequence_completion.sequence_completion_brute_force import (
+    sequence_completion_brute_force,
+)
+from evallm.experiments.sequence_completion.ngram_suffix_heuristic import (
+    ngram_heuristic,
+    ngram_heuristic_with_prefix,
+)
+from evallm.experiments.sequence_completion.sample_sequences import (
+    sample_sequence_completion_problem,
+)
+from evallm.llm.llm import model_specs, run_prompt
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_ngram_scores"
+)
+def compute_ngram_scores(num_seeds, setting):
+    return np.array(
+        [
+            compute_ngram_score(seed, setting=setting, function=ngram_heuristic)
+            for seed in tqdm.trange(num_seeds)
+        ]
+    )
+
+
+def compute_ngram_score(seed, *, setting, function):
+    dfa, examples = _examples(seed, setting)
+    return np.mean(
+        [
+            dfa.accepts_input(prefix + function(sequences, prefix))
+            for sequences, prefix in examples
+        ]
+    )
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_ngram_scores_with_prefix"
+)
+def compute_ngram_scores_with_prefix(num_seeds, setting):
+    return np.array(
+        [
+            compute_ngram_score(
+                seed, setting=setting, function=ngram_heuristic_with_prefix
+            )
+            for seed in tqdm.trange(num_seeds)
+        ]
+    )
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_brute_force_scores"
+)
+def compute_brute_force_scores(num_seeds, setting):
+    return np.array(
+        [
+            compute_brute_force_score(seed, setting=setting)
+            for seed in tqdm.trange(num_seeds)
+        ]
+    )
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_brute_force_score"
+)
+def compute_brute_force_score(seed, setting):
+    dfa, examples = _examples(seed, setting)
+    return np.mean(
+        [
+            dfa.accepts_input(
+                prefix
+                + sequence_completion_brute_force(pack_dfa(dfa), sequences, prefix)[0]
+            )
+            for sequences, prefix in tqdm.tqdm(examples, desc=f"{seed=}")
+        ]
+    )
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_random_baseline_scores"
+)
+def compute_random_baseline_scores(num_seeds, setting):
+    return np.array(
+        [
+            compute_random_baseline_score(seed, setting=setting)
+            for seed in tqdm.trange(num_seeds)
+        ]
+    )
+
+
+def compute_random_baseline_score(seed, *, setting):
+    dfa, examples = _examples(seed, setting)
+    rng = np.random.RandomState(np.random.RandomState(seed).randint(2**32))
+    return np.mean(
+        [
+            dfa.accepts_input(
+                prefix
+                + [
+                    sorted(dfa.input_symbols)[sym]
+                    for sym in rng.choice(
+                        len(dfa.input_symbols), len(sequences[0]) - len(prefix)
+                    )
+                ]
+            )
+            for sequences, prefix in examples
+        ]
+    )
+
+
+def compute_model_scores(num_seeds, setting, model, prompt_fn, *, na_mode="ignore"):
+    prompt = prompt_fn(setting)
+    res = compute_model_score_cached(num_seeds, setting, model, prompt)
+    if na_mode == "ignore":
+        if res[0.5].sum() > 0.25 * sum(res.values()).sum():
+            return np.nan
+        return res[1] / (res[0] + res[1])
+    if na_mode == "zero":
+        return res[1] / (res[0] + res[1] + res[0.5])
+    raise ValueError(f"Invalid na_mode: {na_mode}")
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_model_scores_cached_4",
+    key_function=dict(prompt=lambda prompt: prompt.hash_prompt()),
+)
+def compute_model_score_cached(num_seeds, setting, model, prompt):
+    results = {0: [], 0.5: [], 1: []}
+    for seed in tqdm.trange(num_seeds):
+        scores = compute_model_score(seed, setting=setting, model=model, prompt=prompt)
+        for k, v in scores.items():
+            results[k].append(v)
+    return {k: np.array(v) for k, v in results.items()}
+
+
+@permacache(
+    "evallm/experiments/sequence_completion/sequence_completion_experiments/compute_model_score_4",
+    key_function=dict(prompt=lambda prompt: prompt.hash_prompt()),
+)
+def compute_model_score(seed, *, setting, model, prompt):
+    """
+    Returns counts of correct, incorrect, and N/A completions.
+    """
+    dfa, sequences_prefixes = _examples(seed, setting)
+    is_chat = model_specs[model].is_chat
+    prompts = [
+        prompt.display_prompt(dfa, sequences, prefix, is_chat=is_chat)
+        for sequences, prefix in sequences_prefixes
+    ]
+    responses = run_prompt(model, prompts, prompt.model_kwargs())
+    responses = responses.choices
+    if is_chat:
+        responses = [x.message.content for x in responses]
+    else:
+        responses = [x.text for x in responses]
+    results = Counter(
+        prompt.score_response(dfa, sequences, prefix, response)
+        for (sequences, prefix), response in zip(sequences_prefixes, responses)
+    )
+    return {k: results[k] for k in [0.0, 0.5, 1.0]}
+
+
+def _examples(seed, setting):
+    rng = np.random.RandomState(seed)
+    return sample_sequence_completion_problem(rng, **setting, try_limit=50)
---
+++ evallm/experiments/sequence_completion/sequence_completion_prompt.py
@@ -0,0 +1,112 @@
+from abc import ABC, abstractmethod
+
+import numpy as np
+
+
+class SequenceCompletionPrompt(ABC):
+
+    def __init__(self, num_symbols):
+        self.num_symbols = num_symbols
+
+    @classmethod
+    @abstractmethod
+    def for_setting(cls, setting_kwargs):
+        pass
+
+    @abstractmethod
+    def hash_prompt(self):
+        pass
+
+    @abstractmethod
+    def display_prompt(self, dfa, sequences, prefix, is_chat):
+        pass
+
+    @abstractmethod
+    def score_response(self, dfa, sequences, prefix, response):
+        pass
+
+    @abstractmethod
+    def model_kwargs(self):
+        pass
+
+
+class SequencePromptDirect(SequenceCompletionPrompt):
+    """
+    Sequence prompt that directly displays the sequences to complete.
+    """
+
+    def display_prompt(self, dfa, sequences, prefix, is_chat):
+        prompt = self.preamble()
+        prompt += "\n"
+        prompt += "\n".join(
+            self.format_sequence(dfa, sequence) for sequence in sequences
+        )
+        prompt += "\n\n"
+        prompt += "Complete the following string:"
+        prompt += "\n"
+        prompt += self.format_sequence(dfa, prefix)
+        if is_chat:
+            prompt = {"system": "", "user": prompt}
+        return prompt
+
+    @abstractmethod
+    def preamble(self):
+        pass
+
+    @abstractmethod
+    def format_sequence(self, dfa, sequence):
+        pass
+
+    def score_response(self, dfa, sequences, prefix, response):
+        prediction = ""
+        for tok in response:
+            if tok in dfa.input_symbols:
+                prediction += tok
+            elif tok == " ":
+                continue
+            else:
+                break
+        if not prediction:
+            return 0.5
+        return dfa.accepts_input([*prefix, *prediction])
+
+
+class SequencePromptDirectAlien(SequencePromptDirect):
+
+    def __init__(self, max_out_characters):
+        self.max_out_characters = max_out_characters
+
+    @classmethod
+    def for_setting(cls, setting_kwargs):
+        return cls(
+            max_out_characters=setting_kwargs["num_sequence_symbols"]
+            - setting_kwargs["num_sequence_symbols_prompt"]
+        )
+
+    def hash_prompt(self):
+        return f"SequencePromptDirectAlien({self.max_out_characters})"
+
+    def format_sequence(self, dfa, sequence):
+        return "".join(sequence)
+
+    def preamble(self):
+        return (
+            "The following strings come from an alien language that follows a simple grammar."
+            + " Infer the alien grammar using the example strings. Then, add a suffix to the final string"
+            + f" using between 1 and {self.max_out_characters} characters such that the full string"
+            + " follows the grammar. Output the necessary suffix, and nothing else."
+            + "\n\n"
+            + "Given these grammatically correct example strings:"
+        )
+
+    def model_kwargs(self):
+        return {"max_tokens": self.max_out_characters * 5, "temperature": 0.0}
+
+
+class SequencePromptDirectAlienWithSpaces(SequencePromptDirectAlien):
+
+    def hash_prompt(self):
+        return f"SequencePromptDirectAlienWithSpaces({self.max_out_characters})"
+
+    def format_sequence(self, dfa, sequence):
+        return " ".join(sequence)
