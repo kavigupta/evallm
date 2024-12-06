e7960ef086e9f94dcb80a6f280f3ad93ff17df04
Kavi Gupta <kavi@berkeley.edu>
2024-12-05 21:11:12 -0500

    add metadata and update rendering of main figure (#102)

--- evallm/experiments/main_tables.py
+++ evallm/experiments/main_tables.py
@@ -7,32 +7,46 @@ from evallm.experiments.transducer_plotting import display_acc
 
 @dataclass
 class ModelMetadata:
-    model_size: float = None
-    rlhf_finetuning: bool = None
-    coding_model: bool = None
+    model_size: str | None
+    rlhf_finetuning: bool
+    coding_model: bool
+
+    @property
+    def rlhf_finetuning_check(self):
+        if self.rlhf_finetuning is None:
+            return None
+        return r"\checkmark" if self.rlhf_finetuning else ""
+
+    @property
+    def coding_model_check(self):
+        if self.coding_model is None:
+            return None
+        return r"\checkmark" if self.coding_model else ""
 
 
 metadata_for_models = {
-    "llama3-8B": ModelMetadata(),
-    "llama3-70B": ModelMetadata(),
-    "llama3.1-8B-Instruct": ModelMetadata(),
-    "mistral-nemo-minitron-8B": ModelMetadata(),
-    "mistral-nemo-base-12B": ModelMetadata(),
-    "mistral-nemo-instruct-12B": ModelMetadata(),
-    "starcoder2-15b": ModelMetadata(),
-    "codestral-22B": ModelMetadata(),
-    "deepseek-coder-33b-instruct": ModelMetadata(),
-    "qwen-2.5-coder-7B": ModelMetadata(),
-    "qwen-2.5-coder-instruct-7B": ModelMetadata(),
-    "qwen-2.5-coder-instruct-32B": ModelMetadata(),
-    "gemma-7b": ModelMetadata(),
-    "falcon-7b": ModelMetadata(),
-    "gpt-3.5-instruct": ModelMetadata(),
-    "gpt-3.5-chat": ModelMetadata(),
-    "gpt-4o-mini": ModelMetadata(),
-    "gpt-4o": ModelMetadata(),
-    "claude-3.5": ModelMetadata(),
+    "llama3-8B": ModelMetadata("8.0B", False, False),
+    "llama3-70B": ModelMetadata("70.6B", False, False),
+    "llama3.1-8B-Instruct": ModelMetadata("8.0B", True, False),
+    "mistral-nemo-minitron-8B": ModelMetadata("8.4B", False, False),
+    "mistral-nemo-base-12B": ModelMetadata("12.2B", False, False),
+    "mistral-nemo-instruct-12B": ModelMetadata("12.2B", True, False),
+    "starcoder2-15b": ModelMetadata("16.0B", False, True),
+    "codestral-22B": ModelMetadata("22.2B", False, True),
+    "deepseek-coder-33b-instruct": ModelMetadata("33.3B", True, True),
+    "qwen-2.5-coder-7B": ModelMetadata("7.6B", False, True),
+    "qwen-2.5-coder-instruct-7B": ModelMetadata("7.6B", True, True),
+    "qwen-2.5-coder-instruct-32B": ModelMetadata("32.8B", True, True),
+    "gemma-7b": ModelMetadata("8.5B", False, False),
+    "falcon-7b": ModelMetadata("7.2B", False, False),
+    "gpt-3.5-instruct": ModelMetadata("?", True, False),
+    "gpt-3.5-chat": ModelMetadata("?", True, False),
+    "gpt-4o-mini": ModelMetadata("?", True, False),
+    "gpt-4o": ModelMetadata("?", True, False),
+    "claude-3.5": ModelMetadata("?", True, False),
 }
+
+metadata_baseline = ModelMetadata("--", False, False)
 
 grouped_models = {
     "Baselines": [
@@ -64,7 +78,7 @@ grouped_models = {
         "qwen-2.5-coder-instruct-7B",
         "qwen-2.5-coder-instruct-32B",
     ],
-    "Closed Source": [
+    "Proprietary": [
         "gpt-3.5-instruct",
         "gpt-3.5-chat",
         "gpt-4o-mini",
@@ -74,6 +88,9 @@ grouped_models = {
 }
 
 models_considered = [name for names in grouped_models.values() for name in names]
+models_to_category = {
+    name: category for category, names in grouped_models.items() for name in names
+}
 
 
 def check_all_accounted(names):
@@ -88,7 +105,12 @@ def display_result(results, name):
         [brute_force] = brute_forces
         format_by_mod[brute_force] = r"\cellcolor{lightgray} "
     maximum_acc_model_not_brute_force = max(
-        (x for x in results if "BruteForce" not in x),
+        (
+            x
+            for x in results
+            if "BruteForce" not in x
+            and models_to_category[x] == models_to_category[name]
+        ),
         key=lambda x: np.mean(results[x]),
     )
     format_by_mod[maximum_acc_model_not_brute_force] = r"\bf "
@@ -102,7 +124,7 @@ def main_table_of_results(transducer_results, sequence_completion_results):
     check_all_accounted(sequence_completion_results)
     table = ""
     # model name, parameters, finetuning, coding model, transducer results, sequence completion results
-    table += r"\begin{tabular}{|l|r|c|c|c|c|}" + "\n"
+    table += r"\begin{tabular}{|l|c|c|c|c|c|}" + "\n"
     table += r"\hline" + "\n"
     table += (
         r"\bf Model & \bf Size & \bf RLHF? & \bf Coding? & \bf Transducer & \bf Sequence Completion \\"
@@ -111,14 +133,15 @@ def main_table_of_results(transducer_results, sequence_completion_results):
     for group_name, group in grouped_models.items():
         # double line rule for each header group
         table += r"\hline" + "\n"
-        table += r"\multicolumn{6}{|l|}{ \bf " + group_name + r"} \\" + "\n"
+        table += r"\multicolumn{6}{|c|}{ \bf " + group_name + r"} \\" + "\n"
         table += r"\hline" + "\n"
         for name in group:
-            if group_name == "Baselines":
-                table += f"{name}&&&&"
-            else:
-                metadata = metadata_for_models[name]
-                table += f"{name} & {metadata.model_size} & {metadata.rlhf_finetuning} & {metadata.coding_model} & "
+            metadata = (
+                metadata_baseline
+                if group_name == "Baselines"
+                else metadata_for_models[name]
+            )
+            table += f"{name} & {metadata.model_size} & {metadata.rlhf_finetuning_check} & {metadata.coding_model_check} & "
             table += (
                 f"{display_result(transducer_results, name)} & {display_result(sequence_completion_results, name)} \\\\"
                 + "\n"
