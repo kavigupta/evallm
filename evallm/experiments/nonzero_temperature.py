c29b751107159c603b727cd792c76c1b773d6636
Kavi Gupta <kavi@berkeley.edu>
2025-08-27 19:49:44 -0400

    update cot -> more expl cot (#152)

--- evallm/experiments/nonzero_temperature.py
+++ evallm/experiments/nonzero_temperature.py
@@ -28,7 +28,7 @@ def produce_transducer_results(**kwargs):
             30,
             "Basic",
             "More-Expl",
-            "COT",
+            "More-Expl-COT",
             "Red-Green",
             **kwargs,
         ),
@@ -55,7 +55,7 @@ def produce_sc_results(**kwargs):
             "claude-3.5",
             30,
             # "Basic", "More-Expl" not included becasue they are NaN
-            "COT",
+            "More-Expl-COT",
             "Red-Green",
             na_mode="ignore",
             **kwargs,
