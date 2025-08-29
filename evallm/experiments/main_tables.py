a3dd48f34b55a570fc7a88e978213425bf8bc953
Kavi Gupta <kavi@berkeley.edu>
2025-08-29 12:29:06 -0400

    DFA-COT instead of More-Expl-COT (#161)

--- evallm/experiments/main_tables.py
+++ evallm/experiments/main_tables.py
@@ -208,7 +208,7 @@ def multi_prompt_table_of_results(
 ):
     check_all_accounted(transducer_results)
     check_all_accounted(sequence_completion_results)
-    prompts = ["Basic", "Basic-COT", "More-Expl", "More-Expl-COT", "Red-Green"]
+    prompts = ["Basic", "Basic-COT", "More-Expl", "DFA-COT", "Red-Green"]
     num_basic_prompts = len([p for p in prompts if "Basic" in p])
     assert all(x.startswith("Basic") for x in prompts[:num_basic_prompts])
     num_non_basic_prompts = len(prompts) - num_basic_prompts
